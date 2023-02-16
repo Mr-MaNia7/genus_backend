276ffed52abe5dd6d1ee437215fe0a130a1ecb0f
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2023-02-16 16:12:47 +0300

    Add model

--- music_api/urls.py
+++ music_api/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.getMusics, name = "genus-home"), 
-    path("add/", views.index, name = "upload-sample"),
-    
-    path("login/", views.loginView, name = "login"), 
-    path("register/", views.registerView, name = "register")
+    # path("", views.getMusics, name = "genus-home"), 
+    path("predict/", views.predict, name = "predict"),
+    path('add/',views.add_song,name='add_song'),
+    path('trial/',views.trial,name='trial'),
 
 ]
